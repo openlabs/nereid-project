4fe7a9850fbb1be4310b123cc7cc48911da0e0ef
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2015-01-27 08:35:28 -0500

    Environment variables based configuration
    * Use env variables for TRYTOND_CONFIG and TRYTOND_DB_NAME
    * Use SECRET_PATH for file having secret for app

--- application.py
+++ application.py
@@ -10,44 +10,45 @@ from nereid.sessions import Session
 from raven.contrib.flask import Sentry
 
 CWD = os.path.abspath(os.path.dirname(__file__))
+DATABASE_NAME = os.environ.get('TRYTOND_DB_NAME', 'nereid_project')
+SECRET_PATH = os.environ.get('SECRET_PATH', '.secret')
+
+from trytond.config import CONFIG
+CONFIG.update_etc()
 
 
-def float_to_time(hours):
-    "Converts a float of hours into readable hours and mins"
-    return "%dh %dm" % (hours, (hours * 60) % 60)
-
-CONFIG = dict(
+APP_CONFIG = dict(
 
     # The name of database
-    DATABASE_NAME='nereid_project',
-
-    # Tryton Config file path
-    TRYTON_CONFIG='../etc/trytond.conf',
+    DATABASE_NAME=DATABASE_NAME,
 
     # If the application is to be configured in the debug mode
-    DEBUG=True,
+    DEBUG=False,
 
     # The location where the translations of this template are stored
     TRANSLATIONS_PATH='i18n',
 
-    SECRET_KEY='secretkeygoeshere',
+    SECRET_KEY=open(SECRET_PATH).read(),
 
 )
 # Create a new application
 app = Nereid(static_folder='%s/static/' % CWD, static_url_path='/static')
 
 # Update the configuration with the above config values
-app.config.update(CONFIG)
+app.config.update(APP_CONFIG)
 
 # Initialise the app, connect to cache and backend
 app.initialise()
-app.jinja_env.filters['float_to_time'] = float_to_time
+app.jinja_env.filters[
+    'float_to_time'] = lambda hours: "%dh %dm" % (hours, (hours * 60) % 60)
 app.jinja_env.globals.update({
     'datetime': datetime,
     'guess_mimetype': mimetypes.guess_type,
 })
 
-# Setup the filesystem cache
+# Setup the filesystem cache for session store.
+# This wont work if you scale on more than one servers.
+# Use something like redissessionstore or memcached store
 app.session_interface.session_store = FilesystemSessionStore(
     '/tmp', session_class=Session
 )
