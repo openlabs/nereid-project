b7922c0ff25002ee3533901abad20f14f729a7db
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2015-05-21 12:09:47 +0530

    Fix global search to work with updated ng-material

--- ng-app/app/scripts/directives/global-search.js
+++ ng-app/app/scripts/directives/global-search.js
@@ -5,25 +5,20 @@ angular.module('nereidProjectApp')
   return {
     link: function(scope, element, attrs) {
       var model = $parse(attrs.globalSearch);
-      var inputElement = angular.element(element[0]).find('input');
-      scope.$watch(model, function(value) {
-        if(value === true) {
-          $timeout(function() {
-            inputElement.focus();
-          });
-        }
-      });
+      $timeout(function() {
+        var inputElement = angular.element(element[0]).find('input');
 
-      // Listen to ESC and hide the global search
-      inputElement.bind('keydown keypress', function (event) {
-        if(event.which === 27) {
-          scope.$parent.$apply(function(scope) {
-            scope.showGlobalSearch=false;
-          });
-          scope.$apply(model.assign(scope, false));
-          event.preventDefault();
-        }
-      });
+        // Listen to ESC and hide the global search
+        inputElement.bind('keydown keypress', function (event) {
+          if(event.which === 27) {
+            scope.$parent.$apply(function(scope) {
+              scope.showGlobalSearch=false;
+            });
+            scope.$apply(model.assign(scope, false));
+            event.preventDefault();
+          }
+        });
+      }, 0);
     }
   };
 });
