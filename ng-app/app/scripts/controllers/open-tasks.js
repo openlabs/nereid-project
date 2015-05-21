a6dae01684556a067156ab5ff62dee24cad7ff38
Gaurav Butola <gaurav.butola@openlabs.co.in>
2015-05-21 16:52:16 +0530

    Add kanban view for open tasks view

--- ng-app/app/scripts/controllers/open-tasks.js
+++ ng-app/app/scripts/controllers/open-tasks.js
@@ -2,7 +2,27 @@
 
 angular.module('nereidProjectApp')
 .controller('OpenTasksCtrl', [
-    function() {
-      // TODO:
+    '$scope',
+    'Project',
+    'Helper',
+    'Task',
+    function($scope, Project, Helper, Task) {
+
+      $scope.progressStates = Task.progressStates;
+
+      $scope.getOpenTasks = function(projectId) {
+        var filter = {
+          state: 'opened'
+        };
+        Project.getTasks(projectId, filter)
+          .success(function(result) {
+            $scope.tasks = result.items;
+          })
+          .error(function(reason) {
+            Helper.showDialog('Could not fetch open tasks', reason);
+          });
+      };
+
+
     }
   ]);
