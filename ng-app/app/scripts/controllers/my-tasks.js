99057628704fbb13ef3d54aa05ebb86df2e3f7fc
Gaurav Butola <gaurav.butola@openlabs.co.in>
2015-06-04 14:11:52 +0530

    Show loading bar on my tasks

--- ng-app/app/scripts/controllers/my-tasks.js
+++ ng-app/app/scripts/controllers/my-tasks.js
@@ -10,12 +10,16 @@ angular.module('nereidProjectApp')
       $scope.progressStates = Task.progressStates;
 
       $scope.loadMyTasks = function() {
+        $scope.loadingTasks = true;
         Task.getMyTasks()
           .success(function(result) {
             $scope.tasks = result.items;
           })
           .error(function(reason) {
             Helper.showDialog('Could not fetch your tasks', reason);
+          })
+          .finally(function() {
+            $scope.loadingTasks = false;
           });
       };
 
